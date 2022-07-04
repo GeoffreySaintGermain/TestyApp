06cbce579ec9069945a2eaca7a35c810b4199a9a
Saint-Germain <geoffrey40360@hotmail.fr>
2022-07-04 16:36:15 +0200

    first commit

---
+++ Testy/Testy/TestyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestyApp.swift
+//  Testy
+//
+//  Created by Saint Germain on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
